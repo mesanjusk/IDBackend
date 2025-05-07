71d767162ab7d5230df70be1e367c271877bb2a8
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-07 17:43:51 +0530

    Update

--- models/Image.js
+++ models/Image.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 
 const imageSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type: String, required: true },
-  instaUrl: { type: String },
-  imageUrl: { type: String, required: true },
-}, { timestamps: true });
+  url: String,
+  public_id: String,
+  title: String
+});
 
 export default mongoose.model('Image', imageSchema);
