72276351dd9ed0445e8398d6a3b83d0e9faa888f
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-08 18:02:31 +0530

    Update

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,20 +1,11 @@
-import cloudinary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 dotenv.config();
 
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const uploadToCloudinary = async (filePath, folder) => {
-  try {
-    const result = await cloudinary.v2.uploader.upload(filePath, {
-      folder: folder || 'uploads',
-    });
-    return result;
-  } catch (error) {
-    throw error;
-  }
-};
+export default cloudinary;
