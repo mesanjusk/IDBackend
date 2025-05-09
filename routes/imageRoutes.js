cb22e52240e9796a3c54fa0c085bb76c33b5a83e
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-09 15:12:21 +0530

    Update

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -4,64 +4,49 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import cloudinary from '../utils/cloudinary.js';
 import Image from '../models/Image.js';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const uploadDir = path.join(path.resolve(), 'uploads');
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir); 
+// Cloudinary storage setup
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'mern-images',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + file.originalname;
-    cb(null, uniqueSuffix);  
-  }
 });
-const upload = multer({ storage: storage }); 
+const upload = multer({ storage });
 
 
 // Upload image
-router.post('/', upload.array('images', 10), async (req, res) => {
-  try {
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).send('No files uploaded.');
-    }
+router.post('/',upload.single('image'), async (req, res) => {
+  const { title, category, subcategory,
+      price,
+      instagramUrl,
+      size,
+      religions,
+      seoTitle,
+      seoDescription,
+      seoKeywords } = req.body;
+   const { path, url, filename, public_id } = req.file;
 
-    const imagePaths = req.files.map(file => file.filename);
+  const newImage = new Image({
+    title, category, subcategory,
+      price,
+      instagramUrl,
+      size,
+      religions,
+      seoTitle,
+      seoDescription,
+      seoKeywords
+    url: url || path,
+    public_id: public_id || filename,
+  });
 
-    const newImage = new Image({
-      title: req.body.title,
-      category: req.body.category,
-      subcategory: req.body.subcategory,
-      price: req.body.price,
-      instagramUrl: req.body.instagramUrl,
-      size: req.body.size,
-      religions: req.body.religions,
-      seoTitle: req.body.seoTitle,
-      seoDescription: req.body.seoDescription,
-      seoKeywords: req.body.seoKeywords,
-      images: imagePaths 
-    });
+  await newImage.save();
+  res.status(201).json(newImage);
 
-    const savedImage = await newImage.save();
-    res.status(201).json(savedImage); 
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error', error: error.message });
-  }
 });
 
 // Get all images
