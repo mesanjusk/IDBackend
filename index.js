5a91e9ee0934921556adfb32224da82cf2effda6
sbframee <104187401+sbframee@users.noreply.github.com>
2025-05-10 20:00:01 +0530

    update

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import imageRoutes from './routes/imageRoutes.js';
 import path from 'path';
-import multer from 'multer';
+import listingRoutes from './routes/listing.js';
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Your provided MongoDB URI
@@ -21,13 +22,15 @@ if (!MONGO_URI) {
 
 // Set up CORS, JSON parsing
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' })); // or higher if needed
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Set up static file serving
 app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
 
 // Use the imageRoutes for your image upload logic
 app.use('/api/images', imageRoutes);
+app.use('/api/listings', listingRoutes);
 
 // Connect to MongoDB and start the server
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
