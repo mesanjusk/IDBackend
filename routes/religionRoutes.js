cd25f3f0ba6fb289a2fb72510aea5d41713b7d27
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-10 17:04:38 +0530

    Update

--- routes/religionRoutes.js
+++ routes/religionRoutes.js
@@ -42,4 +42,64 @@ router.post('/add', async (req, res) => {
     }
   });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params; 
+
+  try {
+      const user = await Religion.findById(id);  
+
+      if (!user) {
+          return res.status(404).json({
+              success: false,
+              message: 'Religion not found',
+          });
+      }
+
+      res.status(200).json({
+          success: true,
+          result: user,
+      });
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({
+          success: false,
+          message: 'Error fetching data',
+          error: error.message,
+      });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { name } = req.body;
+   
+    const updateData = { name  };
+    
+    const religion = await Religion.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    );
+
+    if (!religion) {
+      return res.status(404).json({ message: 'Religion not found' });
+    }
+
+    res.json(religion);
+  } catch (err) {
+    console.error('Error updating religion:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Religion.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Religion deleted' });
+  } catch (err) {
+    console.error('Error deleting Religion:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 export default router;
